a7a458ecb47f3bca285049f935ea8aaf53068c14
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-11-04 02:11:04 +0530

    added cart table

---
+++ backend/models/cart.js
@@ -0,0 +1,14 @@
+import pool from "../config/database.js";
+
+export async function cartTable(params) {
+    const query = ` 
+    CREATE TABLE cart IF NOT EXISTS(
+    cart_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      quantity INT DEFAULT 0,
+       created_at TIMESTAMP DEFAULT NOW(),
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    )
+    `
+    await pool.query(query);
+}
