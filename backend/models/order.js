7ea6f277234c7defebaaacdc53879b6d8275a423
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-11-04 02:49:31 +0530

    added order and order_item table

---
+++ backend/models/order.js
@@ -0,0 +1,21 @@
+import pool from "../config/database.js";
+
+export async function orderTable() {
+    const query = ` 
+    CREATE TABLE IF NOT EXISTS orders (
+    order_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+
+    total_amount NUMERIC(10, 2) NOT NULL,
+    order_status VARCHAR(50) DEFAULT 'Pending', -- e.g., Pending, Shipped, Delivered, Cancelled
+
+    payment_method VARCHAR(50), -- e.g., 'Card', 'UPI', 'COD'
+    payment_status VARCHAR(50) DEFAULT 'Unpaid', -- e.g., Paid, Unpaid, Refunded
+
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+`
+    await pool.query(query);
+}
---
+++ backend/models/orderItem.js
@@ -0,0 +1,18 @@
+import pool from "../config/database.js";
+
+export async function orderTable() {
+    const query = ` 
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    order_id UUID REFERENCES orders(order_id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+
+    quantity INT DEFAULT 1,
+    price NUMERIC(10, 2) NOT NULL, -- price per unit at time of purchase
+
+    created_at TIMESTAMP DEFAULT NOW()
+);
+    `
+    await pool.query(query);
+}
