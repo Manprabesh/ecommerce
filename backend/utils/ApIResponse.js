3b108f8c3c4717c0108a759a4368aaa09e195a42
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-11-09 18:35:35 +0530

    added response structure for conistency

---
+++ backend/utils/ApIResponse.js
@@ -0,0 +1,21 @@
+class ApiResponse{
+
+    constructor(message, success, data, file,error){
+        this.success=success;
+        this.message=message;
+        if(data!==null) this.data=data;
+        if(file !== null) this.file = file
+        if(error != null) this.error = error
+    }
+
+    response(message="Request successfull", data=null,file=null){
+        return new ApiResponse (message, true, data, file)
+    }
+
+    reject(message="server error",error=null){
+        return new ApiResponse(message, false, error);
+    }
+}
+
+const api = new ApiResponse()
+export default api
