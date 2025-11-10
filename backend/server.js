68bb108a3c9a49d54481a6d9e627bf5a6c52ad4f
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-11-10 19:26:34 +0530

    server upto date

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,28 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config'
+import cookieParser from "cookie-parser"; 
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
+
+let whitelist = ['http://localhost:5173'];
+let corsOptions = {
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            console.log(origin)
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+}
+app.use(cors(corsOptions))
 
 /**
  * Connect to database and create table
@@ -14,14 +32,22 @@ const client = await pool.connect()
 import { ProductTable } from "./models/product.js";
 import { categoryTable } from "./models/category.js";
 import { userTable } from "./models/user.js";
+import { cartTable } from "./models/cart.js";
+import { orderTable } from "./models/order.js";
+import { orderItemTable } from "./models/orderItem.js";
+import { addressTable } from "./models/address.js";
 
 /**
  * Import router
  */
-import productRouter from "./routes/create_product_router.js";
+import productRouter from "./routes/product_router.js";
 app.use("/api/v1",productRouter)
 import categoryRouter from "./routes/category_router.js";
 app.use("/api/v1",categoryRouter);
+import { userRouter } from "./routes/user_router.js";
+app.use('/api/v1',userRouter);
+import { cartRouter } from "./routes/cart_router.js";
+app.use('/api/v1',cartRouter);
 
 
 
@@ -29,7 +55,11 @@ const PORT = 5000;
 app.listen(PORT, () => {
     ProductTable();
     categoryTable();
-    userTable()
+    userTable();
+    cartTable();
+    orderTable();
+    orderItemTable();
+    addressTable()
     console.log(`Example app listening on port ${PORT}!`);
 });
 
