552fa7831f86665417134b5e31f93bc834da44d2
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-11-10 14:58:20 +0530

    added role

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,8 @@ export async function userTable(){
     CREATE TABLE IF NOT EXISTS users(
     user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     email VARCHAR(100) NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    role VARCHAR(5) DEFAULT user
     )
     `
     await pool.query(query);
